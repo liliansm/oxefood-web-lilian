a62c80e3dd6a1713b02a18101aa6cec22d6a146e
Lilian Souza de Melo <lilianmelo22022005@gmail.com>
2025-05-07 21:39:56 -0300

    integração FormProduto

--- src/views/produto/FormProduto.jsx
+++ src/views/produto/FormProduto.jsx
@@ -1,96 +1,121 @@
+import React, { useState } from "react";
+import axios from "axios";
 import InputMask from 'comigo-tech-react-input-mask';
-import React from "react";
 import { Button, Container, Divider, Form, Icon } from 'semantic-ui-react';
 import MenuSistema from '../../MenuSistema';
 
-export default function FormProduto () {
+export default function FormProduto() {
+    const [codigo, setCodigo] = useState('');
+    const [titulo, setTitulo] = useState('');
+    const [descricao, setDescricao] = useState('');
+    const [valorUnitario, setValorUnitario] = useState('');
+    const [tempoEntregaMinimo, setTempoEntregaMinimo] = useState('');
+    const [tempoEntregaMaximo, setTempoEntregaMaximo] = useState('');
+
+    function salvar() {
+        let produtoRequest = {
+            codigo: codigo,
+            titulo: titulo,
+            descricao: descricao,
+            valorUnitario: parseFloat(valorUnitario.replace(',', '.')),
+            tempoEntregaMinimo: parseInt(tempoEntregaMinimo),
+            tempoEntregaMaximo: parseInt(tempoEntregaMaximo),
+        };
+
+        axios.post("http://localhost:8080/api/produto", produtoRequest)
+            .then((response) => {
+                console.log('Produto cadastrado com sucesso.');
+                alert('Produto cadastrado com sucesso!');
+            })
+            .catch((error) => {
+                console.error('Erro ao incluir o produto:', error);
+                alert('Erro ao incluir o produto.');
+            });
+    }
 
     return (
-
         <div>
-            <MenuSistema tela={'entregador'} />
-
-            <div style={{marginTop: '3%'}}>
-
-                <Container textAlign='justified' >
-
-                    <h2> <span style={{color: 'darkgray'}}> Produto &nbsp;<Icon name='angle double right' size="small" /> </span> Cadastro </h2>
+            <MenuSistema tela={'produto'} />
+            <div style={{ marginTop: '3%' }}>
+                <Container textAlign='justified'>
+                    <h2>
+                        <span style={{ color: 'darkgray' }}>
+                            Produto &nbsp;<Icon name='angle double right' size="small" />
+                        </span>
+                        Cadastro
+                    </h2>
 
                     <Divider />
 
-                    <div style={{marginTop: '4%'}}>
-
+                    <div style={{ marginTop: '4%' }}>
                         <Form>
-
                             <Form.Group widths='equal'>
-
                                 <Form.Input
                                     required
                                     fluid
-                                    label= 'Título'
-                                    placeholder= "Informe o título do produto"
+                                    label='Título'
+                                    placeholder="Informe o título do produto"
                                     maxLength="100"
+                                    value={titulo}
+                                    onChange={e => setTitulo(e.target.value)}
                                 />
 
                                 <Form.Input
                                     required
                                     fluid
                                     label='Código do produto'
-                                    placeholder='Informe o código do produto'>
-                                    {/* <InputMask
-                                        required
-                                        mask="999.999.999-99"
-                                    />  */}
-                                </Form.Input>
-
+                                    placeholder='Informe o código do produto'
+                                    value={codigo}
+                                    onChange={e => setCodigo(e.target.value)}
+                                />
                             </Form.Group>
-                            
-                            <Form.Group widths='equal'>
 
+                            <Form.Group widths='equal'>
                                 <Form.Input
                                     fluid
                                     label='Descrição'
                                     placeholder='Informe a descrição do produto'
-                                    style={{ 
-                                        resize: 'both',  
-                                        overflow: 'auto',  
-                                        minHeight: '80px',  
-                                    }}>
-                                </Form.Input>
-
+                                    value={descricao}
+                                    onChange={e => setDescricao(e.target.value)}
+                                />
                             </Form.Group>
 
                             <Form.Group>
-
                                 <Form.Input
                                     required
                                     fluid
                                     label='Valor Unitário'
-                                    width={6}>
-                                       <InputMask
+                                    width={6}
+                                >
+                                    <InputMask
                                         required
-                                        mask="99,00"
-                                       /> 
+                                        mask="9999,99"
+                                        value={valorUnitario}
+                                        onChange={e => setValorUnitario(e.target.value)}
+                                    />
                                 </Form.Input>
+
                                 <Form.Input
                                     fluid
-                                    label='Tempo de Entrega Minimo em Minutos'
+                                    label='Tempo de Entrega Mínimo (min)'
                                     placeholder='30'
-                                    width={6}>
-                                </Form.Input>
+                                    width={6}
+                                    value={tempoEntregaMinimo}
+                                    onChange={e => setTempoEntregaMinimo(e.target.value)}
+                                />
+
                                 <Form.Input
                                     fluid
-                                    label='Tempo de Entrega Máximo em Minutos'
+                                    label='Tempo de Entrega Máximo (min)'
                                     placeholder='40'
-                                    width={6}>
-                                </Form.Input>
-
+                                    width={6}
+                                    value={tempoEntregaMaximo}
+                                    onChange={e => setTempoEntregaMaximo(e.target.value)}
+                                />
                             </Form.Group>
-                        
                         </Form>
-                        
-                        <div style={{marginTop: '4%'}}>
 
+                        <div style={{ marginTop: '4%' }}>
                             <Button
                                 type="button"
                                 inverted
@@ -98,11 +123,12 @@ export default function FormProduto () {
                                 icon
                                 labelPosition='left'
                                 color='orange'
+                                onClick={() => window.location.href = '/list-produto'}
                             >
                                 <Icon name='reply' />
                                 Listar
                             </Button>
-                                
+
                             <Button
                                 inverted
                                 circular
@@ -110,19 +136,15 @@ export default function FormProduto () {
                                 labelPosition='left'
                                 color='blue'
                                 floated='right'
+                                onClick={salvar}
                             >
                                 <Icon name='save' />
                                 Salvar
                             </Button>
-
                         </div>
-
                     </div>
-                    
                 </Container>
             </div>
         </div>
-
     );
-
 }
